461c6fcebfe45c25cbd1b61e33d17afc215a6ff3
Marko Hlebar <marko.hlebar@gmail.com>
2020-03-23 14:19:42 +0100

    Initial Commit

---
+++ IdenfyKycPlugin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IdenfyKycPlugin
+//
+//  Created by Marko Hlebar on 23/03/2020.
+//  Copyright © 2020 Sonect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ IdenfyKycPlugin/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  IdenfyKycPlugin
+//
+//  Created by Marko Hlebar on 23/03/2020.
+//  Copyright © 2020 Sonect. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
